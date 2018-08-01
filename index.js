25f1023a4c573ffb2abc8b677f950ae708564e62
Ricky Liu <rorix12321@gmail.com>
2018-08-01 01:34:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
+<<<<<<< HEAD
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 function nowServing(line) {
@@ -13,5 +14,11 @@ function nowServing(line) {
 function currentLine(line) {
   if (line.length>0){return `The line is currently: 1. ${line[0]}, 2. ${line[1]}, 3. ${line[2]}`
 }
+=======
+  return `Welcome, ${name}. You are number ${katzDeliLine.length+1} in line.`
+}
+function nowServing(line, name) {
+  if (line.length>0){line.shift}
+>>>>>>> c2da7bd8afa475a4c0a7fc46f0068eebec931120
   else {return "The line is currently empty."}
 }
